4d000d37b388ac188e5d71bab2b4a518bdf7f79d
Win Eiwwongcharoen <win.eiwwongcharoen@gmail.com>
2021-05-22 14:08:03 +0700

    first commit

--- pages/_app.js
+++ pages/_app.js
@@ -1,7 +1,15 @@
-import '../styles/globals.css'
+import '../styles/globals.css';
+// import liff from '@line/liff';
+import * as React from 'react';
 
 function MyApp({ Component, pageProps }) {
-  return <Component {...pageProps} />
+  // console.log('liff', liff);
+  React.useEffect(async () => {
+    const liff = (await import('@line/liff')).default;
+    console.log('liff:', liff);
+  }, []);
+
+  return <Component {...pageProps} />;
 }
 
-export default MyApp
+export default MyApp;
